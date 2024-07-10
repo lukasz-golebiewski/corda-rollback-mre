63e05b46e7b276385dabdfced2d5f5d1c5d1dae0
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2024-07-10 12:06:13 +0200

    Fixes gradle

---
+++ src/main/kotlin/com/example/MainFlow.kt
@@ -0,0 +1,28 @@
+package com.example
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.TimeWindow
+import net.corda.core.flows.FinalityFlow
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.FlowSession
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.NotaryError
+import net.corda.core.flows.NotaryException
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
+
+@InitiatingFlow
+@StartableByRPC
+open class MainFlow() : FlowLogic<Unit>() {
+
+    @Suspendable
+    override fun call(): Unit {
+        logger.info("${this::class.simpleName} has started.")
+
+        logger.info("${this::class.simpleName} has finished.")
+    }
+}
